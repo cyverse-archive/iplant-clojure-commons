c7185258e5f1558e83178a0403f9c96964bdf8c5
John Wregglesworth <wregglej@iplantcollaborative.org>
2012-01-09 13:49:04 -0700

    Added code for grabbing configuration options from Zookeeper.

--- project.clj
+++ project.clj
@@ -2,7 +2,10 @@
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/data.json "0.1.1"]
+                 [org.clojure/tools.logging "0.2.3"]
+                 [zookeeper-clj "0.9.1"]
                  [clj-http "0.2.5"]
+                 [log4j/log4j "1.2.16"]
                  [org.mongodb/mongo-java-driver "2.6.3"]
                  [org.apache.httpcomponents/httpclient "4.1.2"]]
   :dev-dependencies [[swank-clojure "1.3.2"]])
