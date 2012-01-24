119a1125c179a964b4e977a153de9386076591e9
Dennis Roberts <dennis@iplantcollaborative.org>
2012-01-24 14:42:53 -0700

    CORE-2820: add support for Apache Commons Config to clojure-commons.props

--- project.clj
+++ project.clj
@@ -7,5 +7,6 @@
                  [clj-http "0.2.5"]
                  [log4j/log4j "1.2.16"]
                  [org.mongodb/mongo-java-driver "2.6.3"]
-                 [org.apache.httpcomponents/httpclient "4.1.2"]]
+                 [org.apache.httpcomponents/httpclient "4.1.2"]
+                 [commons-configuration/commons-configuration "1.7"]]
   :dev-dependencies [[swank-clojure "1.3.2"]])
