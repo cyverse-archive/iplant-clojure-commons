d83d7a311b6ef0c5b9662b9b5721cf5cf3f96655
John Wregglesworth <wregglej@iplantcollaborative.org>
2012-10-12 13:27:12 -0700

    Added code to get provenance stuff working, maybe.

--- src/clojure_commons/provenance.clj
+++ src/clojure_commons/provenance.clj
@@ -6,12 +6,12 @@
 
 (defn prov-map
   "Creates a map that can be passed to the provenance related functions.
-   The proxy-user and data keys area optional and will be set to nil if they're
-   not provided."
+   The proxy-user and data keys area optional and will be set to nil if
+   they're not provided."
   [url id user service event category & [proxy-user data]]
   (hash-map
    :prov-url url
-   :object-id id
+   :id id
    :user user
    :service service
    :event event
@@ -36,8 +36,8 @@
   [prov-url lp-map]
   (client/put
    prov-url
-   {:throw-exceptions false}
-   {:content-type :json
+   {:throw-exceptions false
+    :content-type :json
     :body (json/json-str lp-map)}))
 
 (defn call-add-object
@@ -64,7 +64,7 @@
 (defn log
   "Takes in a map created by (prov-map) and logs it to the provenance
    service."
-  [{:keys [prov-url object-id user service event category proxy-user data]
+  [{:keys [prov-url id user service event category proxy-user data]
     :as log-map}]
   (log/warn prov-url)
   (let [lp-map (clean-prov-map log-map)
@@ -101,7 +101,7 @@
 (defn exists?
   "Checks to see if an object is already registered or not."
   [prov-url obj-id]
-  (let [exists-url    (str (url prov-url "0.1" "object" (url-encode obj-id)))
+  (let [exists-url (str (url prov-url "0.1" "object" (url-encode obj-id)))
         lookup-status (:status (call-lookup-object exists-url))]
     (cond
      (= lookup-status 404)      false
