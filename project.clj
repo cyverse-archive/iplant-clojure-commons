34578b0133bbcc681a5dd2d8f4403e96ec59697f
Dennis Roberts <dennis@iplantcollaborative.org>
2013-01-29 14:25:15 -0700

    CORE-3924: migrated to clj-http version 0.6.3

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject org.iplantc/clojure-commons "1.3.3-SNAPSHOT"
+(defproject org.iplantc/clojure-commons "1.4.0-SNAPSHOT"
   :description "Common Utilities for Clojure Projects"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/data.json "0.1.1"]
                  [org.clojure/tools.logging "0.2.3"]
                  [zookeeper-clj "0.9.1"]
-                 [clj-http "0.5.6"]
+                 [clj-http "0.6.3"]
                  [com.cemerick/url "0.0.7"]
                  [com.github.drsnyder/beanstalk "1.0.0-clj14"]
                  [log4j/log4j "1.2.16"]
