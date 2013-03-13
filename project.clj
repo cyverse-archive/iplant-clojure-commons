70e9571883eb1a93871cdb326da6c763a5c73c14
John Wregglesworth <wregglej@iplantcollaborative.org>
2013-03-13 14:05:19 -0700

    Bumped version to 1.4.1-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.iplantc/clojure-commons "1.4.0-SNAPSHOT"
+(defproject org.iplantc/clojure-commons "1.4.1-SNAPSHOT"
   :description "Common Utilities for Clojure Projects"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.logging "0.2.3"]
