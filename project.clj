7879ad7c9861c2b41698ed722b956929483170de
Tony Edgin <tedgin@iplantcollaborative.org>
2013-03-19 14:26:11 -0700

    upped dependencies and created version 1.4.1-SNAPSHOT
    clojure -> 1.5.0
    clj-http -> 0.6.5
    log4j -> 1.2.17
    mongo-java-driver -> 2.10.1
    httpclient -> 4.2.3
    commons-configuration -> 1.8
    cheshire -> 5.0.2

--- project.clj
+++ project.clj
@@ -1,16 +1,16 @@
-(defproject org.iplantc/clojure-commons "1.4.0-SNAPSHOT"
+(defproject org.iplantc/clojure-commons "1.4.1-SNAPSHOT"
   :description "Common Utilities for Clojure Projects"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/tools.logging "0.2.3"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
+                 [org.clojure/tools.logging "0.2.6"]
                  [zookeeper-clj "0.9.1"]
-                 [clj-http "0.6.3"]
+                 [clj-http "0.6.5"]
                  [clj-http-fake "0.4.1"]
                  [com.cemerick/url "0.0.7"]
                  [com.github.drsnyder/beanstalk "1.0.0-clj14"]
-                 [log4j/log4j "1.2.16"]
+                 [log4j/log4j "1.2.17"]
                  [slingshot "0.10.3"]
-                 [org.mongodb/mongo-java-driver "2.6.3"]
-                 [org.apache.httpcomponents/httpclient "4.2.2"]
-                 [commons-configuration/commons-configuration "1.7"]
-                 [cheshire "5.0.1"]]
+                 [org.mongodb/mongo-java-driver "2.10.1"]
+                 [org.apache.httpcomponents/httpclient "4.2.3"]
+                 [commons-configuration/commons-configuration "1.8"]
+                 [cheshire "5.0.2"]]
   :profiles {:test {:resource-paths ["resources" "test-resources"]}})
