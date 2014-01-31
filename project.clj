3ac5704c1742e794d135db14553e6b9f808a2cf5
Dennis Roberts <dennis@iplantcollaborative.org>
2014-01-30 19:11:10 -0700

    added some elements that are required by sonatype to project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,14 @@
-(defproject org.iplantc/clojure-commons "1.4.7"
+(defproject org.iplantc/clojure-commons "1.4.8-SNAPSHOT"
   :description "Common Utilities for Clojure Projects"
+  :url "http://www.iplantcollaborative.org"
+  :license {:name "BSD"
+            :url "http://iplantcollaborative.org/sites/default/files/iPLANT-LICENSE.txt"}
+  :scm {:connection "scm:git:git@github.com:iPlantCollaborativeOpenSource/metadactyl.git"
+        :developerConnection "scm:git:git@github.com:iPlantCollaborativeOpenSource/metadactyl.git"
+        :url "git@github.com:iPlantCollaborativeOpenSource/metadactyl.git"}
+  :pom-addition [:developers
+                 [:developer
+                  [:url "https://github.com/orgs/iPlantCollaborativeOpenSource/teams/iplant-devs"]]]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.logging "0.2.6"]
                  [zookeeper-clj "0.9.1"]
@@ -12,4 +21,8 @@
                  [org.apache.httpcomponents/httpclient "4.2.3"]
                  [commons-configuration/commons-configuration "1.8"]
                  [cheshire "5.0.2"]]
+  :repositories [["sonatype-nexus-snapshots"
+                  {:url "https://oss.sonatype.org/content/repositories/snapshots"}]
+                 ["sonatype-nexus-staging"
+                  {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2/"}]]
   :profiles {:test {:resource-paths ["resources" "test-resources"]}})
